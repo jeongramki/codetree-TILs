c47adef615abde0febcef9e95c0306c7595fac20
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-04-30 19:53:38 +0900

    Title: 학점 계산기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240430/학점 계산기/credit-calculator.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+arr = map(float,input().split())
+
+sum = 0
+for elem in arr : 
+    sum += elem 
+
+
+avr = sum/num
+
+print(f"{avr:.1f}")
+
+if avr > 4.0 : print('Perfect')
+elif (4> avr > 3) : print('Good')
+else : print('Poor')
