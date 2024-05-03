f690754e4af3965b57ed62e3d71133133575d690
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-05-03 17:23:13 +0900

    Title: 오름 내림차순 정렬, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,13 @@
+num = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+for elem in arr : 
+    print(elem, end = ' ')
+print()
+
+arr.sort(reverse = True)
+
+for elem in arr : 
+    print(elem, end = ' ')
