ae2597696890705dd53010fb0a7b74ee614c4f62
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-05-03 18:29:53 +0900

    Title: 2개씩 그룹짓기, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/2개씩 그룹짓기/group-of-pairs.py
+++ 240503/2개씩 그룹짓기/group-of-pairs.py
@@ -3,8 +3,9 @@ num = int(input())
 arr = list(map(int, input().split()))
 
 arr.sort()
+
 sum = [] 
 for i in range(num) : 
-    sum.append (arr[i-0] + arr[num+1-i])
+    sum.append (arr[i] + arr[2*num-1-i])
 
 print(max(sum))
