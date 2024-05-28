6f6c4cff1dd8679e5c5e4c8b297e43f1a3a899b9
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-05-28 16:18:22 +0900

    Title: 시력 검사, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240528/시력 검사/eye-test.py
+++ 240528/시력 검사/eye-test.py
@@ -2,5 +2,5 @@ L = float(input())
 R =float(input())
 
 if L>=1 and R>=1 : print('High')
-elif 1 > L >= 0.5 and 1 > R >= 0.5 : print('Middle')
+elif L >= 0.5 and R >= 0.5 : print('Middle')
 else : print('Low')
