22c448448e26af535488e2c44032690d17631fc0
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-05-28 12:44:17 +0900

    Title: 알파벳에 따른 평가, Time: 203ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240528/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,8 @@
+ss = input()
+
+if ss == 'S' : print('Superior')
+elif ss == 'A' : print('Excellent')
+elif ss == 'B' : print('Good')
+elif ss == 'C' : print('Usually')
+elif ss == 'D' : print('Effort')
+else : print('Failure')
