bf94485dcee1eb51bdc2b505b034f9329d45cadb
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-06-04 12:23:38 +0900

    Title: 윤년 횟수 구하기, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240604/윤년 횟수 구하기/number-of-leap-years.py
+++ 240604/윤년 횟수 구하기/number-of-leap-years.py
@@ -1,7 +1,7 @@
 a = int(input())
 num =0
 for i in range(1,a+1) : 
-    if i % 4 == 0 and i%100!=0: num +=1
-    elif i % 4 == 0 and i%100==0: num += 0 
     
+    if i % 100  == 0 and i%400!=0: num += 0 
+    elif i % 4 == 0: num +=1
 print(num)
