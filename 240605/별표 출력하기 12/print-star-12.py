2f2468489232d6eaebeea991f9b9eeefe4346c98
jeongramki <168340522+jeongramki@users.noreply.github.com>
2024-06-05 17:30:05 +0900

    Title: 별표 출력하기 12, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240605/별표 출력하기 12/print-star-12.py
+++ 240605/별표 출력하기 12/print-star-12.py
@@ -1,8 +1,15 @@
 n=int(input())
 
-for i in range(n):
-    for j in range(n) : 
-        if j < i : print(' ',end = ' ')
-        elif j %2 == 0 and j>= i : print('*',end = ' ')
-        elif j%2 ==1 and j>=i : print(' ',end=' ')
+for i in range(n) : 
+    if i == 0 : 
+        for _ in range(n) : print('*', end = ' ')
+
+    else : 
+        for j in range(n) :
+            if j < i : print(' ', end = ' ' )
+            elif (j>=i) and (j%2==1) : print('*',end=' ')
+            elif (j>=i) and (j%2==0) : print(' ',end=' ')
+            else : print('')
+
+         
     print('')
